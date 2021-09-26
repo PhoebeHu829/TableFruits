b37cad726144a46693f1caad8b11a1c07b2aec4f
Phoebe <huxiaodan829@gmail.com>
2021-09-25 21:56:11 -0700

    A2

--- TableFruits/ViewController.swift
+++ TableFruits/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
 
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self;
+        tblView.dataSource = self;
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: <#T##IndexPath#>)
+        
+        return cell;
+    }
+    
 }
 
